b7f04b4961bfbd337bdd2f3fa633fe741931b957
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-10-23 16:31:40 +0000

    Add header and footer all pages

---
+++ header.php
@@ -0,0 +1,33 @@
+<header>
+      <nav class="navbar navbar-expand-md navbar-dark">
+        <a class="navbar-brand" href="./index.html">
+            <img src="./images/logo.png" alt="Logo Restaurant" class="img-fluid" style="max-height: 50px;">
+            <span style="font-size: 1em;">La Trattoria Bella Italia</span> 
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Restaurant</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="menu.html">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="bar.html">Bar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="events.html">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="booking.html">Booking</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
